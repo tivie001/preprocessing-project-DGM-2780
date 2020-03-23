118255d21b0966a738364d089d8d99f930a353bf
tylerivie <tyler.ivie@upma.org>
2020-03-22 21:08:45 -0600

    Created less file for mixins. Made code responsive.

--- less-assignment/js/index.js
+++ less-assignment/js/index.js
@@ -1,1 +1,14 @@
 // Custom JS here
+function sendToHome() {
+    location.href = "index.html";
+}
+function sendToBikePage() {
+    location.href = "shopBikes.html";
+}
+function openMobileNav() {
+    document.getElementById("mySidebar").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+}
